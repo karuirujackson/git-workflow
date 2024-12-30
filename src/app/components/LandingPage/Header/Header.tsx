96c6037041746f1e98364a4dec86455a5ae4a23c
Karuiru Jackson <jkjackaruiru641@gmail.com>
2024-12-30 20:13:30 +0300

    Addded a Header component

---
+++ src/app/components/LandingPage/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+        <div className="flex flex-col gap-3 pt-16">
+            <p className="text-3xl leading-10 font-[inter] font-extrabold">Hey there, here is all you need to join a project</p>
+            <p className="text-xl leading-7 font-[inter] font-medium text-[#6B7280]">Ensure you read all the documentation on Notion before picking a task to work on.</p>
+      </div>
+    )
+}
+
+export default Header;
